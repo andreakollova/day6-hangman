0658563748ff89bd5cb6bf07006f436d450dcc03
andreakollova <andrea_kollova@hotmail.com>
2025-04-26 08:27:11 +0200

    Hangman = Step 4 - FINAL

--- main.py
+++ main.py
@@ -1,6 +1,59 @@
+stages = [r'''
+ +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''', r'''
+ +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', r'''
++---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', r'''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', r'''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', r'''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      | 
+=========''', r'''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''']
+
 import random
 #1 RANDOM WORD IS GENERATED AND PRINTED, ALSO SPLIT INTO LETTERS
 word_list = ["apple", "house", "table", "chair", "river", "stone", "cloud", "beach", "smile", "music", "grass", "water", "bread", "light", "flower", "heart", "dream", "honey", "orange", "sunny"]
+
+lives = 6
 
 random_word = random.choice(word_list)
 print(random_word)
@@ -33,6 +86,14 @@ while not gameover:
             display += "_"
     print(display)
 
-if "_" not in display:
-    gameover = True
-    print("You won!")
+    if guess not in letters:
+        lives -= 1
+        if lives == 0:
+            gameover = True
+            print("You lose!")
+
+    if "_" not in display:
+        gameover = True
+        print("You won!")
+
+    print(stages[lives])
